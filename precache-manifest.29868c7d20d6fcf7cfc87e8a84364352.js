9118a38a7dbf62f3055af79da63dc0323e3c1a1d
kyupkyup <rudduqdl@naver.com>
2020-02-18 00:25:48 +0900

    Updates

---
+++ precache-manifest.29868c7d20d6fcf7cfc87e8a84364352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b9a45d82f03b6c5e27cbe42df4a655",
+    "url": "/originals-frontend/index.html"
+  },
+  {
+    "revision": "4b99d1d4dc459a398006",
+    "url": "/originals-frontend/static/css/2.ddc88af7.chunk.css"
+  },
+  {
+    "revision": "4b99d1d4dc459a398006",
+    "url": "/originals-frontend/static/js/2.beabc842.chunk.js"
+  },
+  {
+    "revision": "43937419b0c57d53f85a3eec27f86e5e",
+    "url": "/originals-frontend/static/js/2.beabc842.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66ad8dc852124e4b66b8",
+    "url": "/originals-frontend/static/js/main.019689d9.chunk.js"
+  },
+  {
+    "revision": "8b96c3c695da39795a50",
+    "url": "/originals-frontend/static/js/runtime-main.a7b3bb94.js"
+  }
+]);
