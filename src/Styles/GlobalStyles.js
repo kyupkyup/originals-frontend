1cebce8a2d191bd12912d3c473dd80f924daa3bb
kyupkyup <rudduqdl@naver.com>
2020-02-23 02:09:53 +0900

    sadf

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -3,14 +3,14 @@ import reset from "styled-reset";
 
 export default createGlobalStyle`
     ${reset};
-    @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700');
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap');
 
     * {
         box-sizing:border-box;
     }
     body{
-        background-color:${props=> props.theme.ligthGray5};
-        color:${props=> props.theme.blackColor};
+        background-color:${props => props.theme.ligthGray5};
+        color:${props => props.theme.blackColor};
         font-size:14pt;
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 
